97a5cccc945dff8c9722f4f2c79770cbc28df20e
Göksun Gürel <goksungurell@gmail.com>
2025-05-04 14:46:37 +0300

    buton

--- Assets/Scripts/button.cs
+++ Assets/Scripts/button.cs
@@ -1,19 +1,34 @@
 using UnityEngine;
 
-public class button : MonoBehaviour
+public class Button : MonoBehaviour
 {
-    public Animator buttonAnimator;
-    private bool basildi = false;
+    public Animator buttonAnimator;  // Animator referansı
+    private bool basildi = false;   // Buton basıldığını kontrol eder
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!basildi && other.CompareTag("Player"))
         {
-            basildi = true;
+            basildi = true;  // Buton basıldığını işaretle
+            Debug.Log("Butona basıldı!");
+
+            // Animator varsa "Pressed" trigger'ını çalıştır
             if (buttonAnimator != null)
             {
-                buttonAnimator.SetTrigger("Pressed");
+                buttonAnimator.SetTrigger("Pressed");  // Animasyonu tetikle
+                buttonAnimator.SetBool("isPressed", true);  
+            }
+            else
+            {
+                Debug.LogError("Button animator referansı eksik!");
             }
         }
+    }
+
+    // Animasyon bitene kadar butonu tekrar basılabilir yapmamak için:
+    private void ResetButton()
+    {
+        // Buton animasyonu tamamlandıktan sonra basıldı durumunu sıfırla
+        basildi = false;
     }
 }
